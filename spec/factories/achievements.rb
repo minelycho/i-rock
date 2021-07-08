7ae2f8246e28a264efaa9f39e7d6d4b5e9af3eb0
Minerva <romanagrimm7@gmail.com>
2021-07-08 14:25:37 -0600

    factory bot

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -1,9 +1,14 @@
 FactoryBot.define do
   factory :achievement do
-    title { "MyString" }
+    sequence(:title) { |n| "Achievement #{n}"}
     description { "MyText" }
-    privacy { 1 }
+    privacy { Achievement.privacies[:private_access] }
     features { false }
-    cover_image { "MyString" }
+    cover_image { "some_file.png" }
+
+  factory :public_achievement do 
+    privacy {Achievement.privacies[:public_access]}
+
+    end
   end
 end
