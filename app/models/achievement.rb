9985a93ee7b5cee45c0a49889d8b3e642e4eb9d7
Minerva <romanagrimm7@gmail.com>
2021-08-02 11:09:36 -0600

    Test validations

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -4,10 +4,21 @@ class Achievement < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
+  #validates :title, uniqueness: true
+  validate :unique_title_for_one_user
 
   enum privacy: [ :public_access, :private_access, :friends_access ]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
+
+  private
+
+  def unique_title_for_one_user
+    existing_achivement = Achievement.find_by(title: title)
+    if existing_achivement && existing_achivement.user == user
+      errors.add(:title, 'you can\'t have two achievements with the same title')
+    end
   end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'require title' do
+      achievement = Achievement.new(title: '')
+=begin
+  #alternative syntax
+      achievement.valid?
+      expect(achievement.errors[:title]).to include('can\'t be blank')
+      expect(achievement.errors[:title]).not_to be_empty
+=end
+      expect(achievement.valid?).to be_falsy
+    end
+
+    it 'renders title to be unique for one user' do
+      user = FactoryBot.create(:user)
+      first_achievement = FactoryBot.create(:public_achievement, title: 'First Achivement', user: user)
+      new_achievement = Achievement.new(title: 'First Achivement', user: user)
+      expect(new_achievement.valid?).to be_falsy
+    end
+
+    it 'allows different users to have achievements with identical titles' do
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      first_achievement = FactoryBot.create(:public_achievement, title: 'First Achivement', user: user1)
+      new_achievement = Achievement.new(title: 'First Achivement', user: user2)
+      expect(new_achievement.valid?).to be_truthy
+    end
+  end
 end
