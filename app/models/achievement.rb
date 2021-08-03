73013b049a8ee01be06fc8d63223cce990fbc8b3
Minerva <romanagrimm7@gmail.com>
2021-08-03 11:30:28 -0600

    test Shoulda-Matchers gem

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -5,8 +5,10 @@ class Achievement < ApplicationRecord
 
   validates :title, presence: true
   validates :user, presence: true
-  #validates :title, uniqueness: true
-  validate :unique_title_for_one_user
+  validates :title, uniqueness: {
+    scope: :user_id,
+    message: ('you can\'t have two achievements with the same title')
+  }
 
   enum privacy: [ :public_access, :private_access, :friends_access ]
 
@@ -16,10 +18,5 @@ class Achievement < ApplicationRecord
 
   private
 
-  def unique_title_for_one_user
-    existing_achivement = Achievement.find_by(title: title)
-    if existing_achivement && existing_achivement.user == user
-      errors.add(:title, 'you can\'t have two achievements with the same title')
-    end
-  end
+
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -2,48 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
   describe 'validations' do
-    it 'require title' do
-      achievement = Achievement.new(title: '')
-=begin
-  #alternative syntax
-      achievement.valid?
-      expect(achievement.errors[:title]).to include('can\'t be blank')
-      expect(achievement.errors[:title]).not_to be_empty
-=end
-      expect(achievement.valid?).to be_falsy
-    end
-
-    it 'renders title to be unique for one user' do
-      user = FactoryBot.create(:user)
-      first_achievement = FactoryBot.create(:public_achievement, title: 'First Achivement', user: user)
-      new_achievement = Achievement.new(title: 'First Achivement', user: user)
-      expect(new_achievement.valid?).to be_falsy
-    end
-
-    it 'allows different users to have achievements with identical titles' do
-      user1 = FactoryBot.create(:user)
-      user2 = FactoryBot.create(:user)
-      first_achievement = FactoryBot.create(:public_achievement, title: 'First Achivement', user: user1)
-      new_achievement = Achievement.new(title: 'First Achivement', user: user2)
-      expect(new_achievement.valid?).to be_truthy
-    end
+    it { should validate_presence_of(:title) }
+    #it { should validate_uniqueness_of(:title).scoped_to(:user_id)}
+    it { expect(FactoryBot.create(:public_achievement)).to validate_uniqueness_of(:title).
+         scoped_to(:user_id).with_message('you can\'t have two achievements with the same title')}
+    it { should validate_presence_of(:user)}
+    it { should belong_to(:user)}
   end
 
-  it 'belongs to user' do
-    achievement = Achievement.new(title: 'Some title', user: nil)
-    expect(achievement.valid?).to be_falsy
-  end
-
-  it 'has belongs_to user association' do
-    # 1 approach
-    user = FactoryBot.create(:user)
-    achievement = FactoryBot.create(:public_achievement, user: user)
-    expect(achievement.user).to eq(user)
-
-
-    # 2 approach
-    u = Achievement.reflect_on_association(:user)
-    expect(u.macro).to eq(:belongs_to)
-  end
 
 end
